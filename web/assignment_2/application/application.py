6bea8a4ed1349e3c579bc3b3a49e1a94e0a919b0
Macarthur Inbody <133794m3r@gmail.com>
2020-06-26 16:07:02 -0400

    Moving towards finally getting this thing working.

--- web/assignment_2/application/application.py
+++ web/assignment_2/application/application.py
@@ -1,21 +1,44 @@
 import os
 
-from flask import Flask,jsonify,request
+from flask import Flask,jsonify,request,render_template
 import time,json
 from flask_socketio import SocketIO, emit,join_room,leave_room
+import logging
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socket = SocketIO(app)
-
+#the total "global" channels.
 channels_list = ["main"]
+#the private channels that people can create.
+#each one has a name and also the password to join it.
 """
-User Private messages.
-
-It's a dictionary that looks like this.
-user_pms[<username>] = { num_msgs:<number of messages>,
-						 msgs:[<list>,<of>,<msgs>]
-						}
+There are 2 keys. One that maps the channel's true name(name given by the user
+16 bits from socke.io sid). Then the other major key is the passwords. An 
+example is below.
+{
+	"names":{
+			"test_channela1345df":"test_channel"
+	}
+	"passwords":{
+			"test_channel1345df":"password"
+	}
+}
+Basically the "passwords" dictionary holds the channel's truename and the
+password to join it. Whereas names maps it in the exact opposite fashion. When
+a user gives someone an invite what they are actually giving them is the channel
+name. Plus the other data is embedded within the link when they join it.
+"""
+private_channels = {"names":{},
+                    "passwords":{}
+                    }
+"""
+User Private messages. It's a list of all current PMs that users are currently 
+doing.  It's name is simply the concentation of the sid(s) of the 2 users and 
+it. Then it contains the 2 session ids that we should broadcast to so that don't
+let someone join the room w/o access.  
 """
 user_pms = {}
 
@@ -25,6 +48,16 @@ It basically looks like this. Where request.sid is the socketio session id.
 users[<username>] = request.sid
 """
 users = {}
+
+"""
+A dictionary that holds the rooms that the user has passwords to/has connected 
+to previously. It holds the rooms that they can connect to automatically upon 
+reconnecting. It holds the rooms that they have passwords to.
+
+users_rooms = { <username>:[<list>,<of>,<roms>]}
+"""
+users_rooms = {}
+
 """
 Here we have all of the channel messages each channel's messages as their own
 named item. This is to avoid having everything sent to the user each time a 
@@ -39,6 +72,26 @@ channels_msgs = {
 }
 #How many messages we're going to allow each channel to have.
 msg_limit = 100
+
+def private_channels_info(rooms_list):
+	#private_channels.items()
+	private_info=[]
+	tmp={}
+	for room in rooms_list:
+		tmp[room]={}
+		tmp[room]['name']=private_channels['names'][room]
+		tmp[room]['password']=private_channels["passwords"][room]
+		private_info.append(tmp)
+
+	return private_info
+
+def update_room_msg(data,room):
+	msg = {'text': data['msg'], 'username': data['username'], 'time': data['time']}
+	if channels_msgs[room]['num_msgs'] >= 50:
+		channels_msgs[room]['msgs'].pop(0)
+	else:
+		channels_msgs[room]['num_msgs'] += 1
+	channel_msgs[room]["msgs"].append(msg)
 
 @app.route("/")
 def index():
@@ -56,42 +109,62 @@ def get_messages():
 def get_users():
 	pass
 
-
 @socket.on("connect")
 def connect():
-	emit("get channels",{"channels":channels})
+	print('connecting')
+	emit("connect",{'msg':'a'})
+
+@socket.on("rejoin")
+def reconnect(data):
+	channel=channels_list[0]
+	join_room(channel)
+	username=data['username']
+	private_rooms=users_rooms.get('username')
+	if private_rooms:
+		private_rooms=private_channels_info(private_rooms)
+	else:
+		private_rooms={}
+	emit("update_channels",{"channels":channels_list,"private_channels":private_rooms},room=request.sid)
+	emit("joined",{"success":True,"channel_msgs":channels_msgs[channel]['msgs']},room=request.sid)
 
 @socket.on("join")
 def join(data):
-	if data is "":
-		send("joined",{success:False,"error":"Response was no received","channel_msgs":""},room=request.sid)
+	if data == "":
+		send("joined",{success:False,"error":"Response was not received","channel_msgs":""},room=request.sid)
 	room = data["channel"]
 	if room not in channels_list:
-		send("joined",{success:False,"error":"Invalid room selected.","channel_msgs":""},room=request.sid)
+		if room not in private_channels:
+			send("joined",{success:False,"error":"Invalid room selected.","channel_msgs":""},room=request.sid)
+		else:
+			if data["password"] != private_channels[room]["password"]:
+				send("joined", {success: False, "error": "Invalid password provided.", "channel_msgs": ""}, room=request.sid)
+
 	join_room(room)
 
-	if channels_msgs["main"]["num_msgs"] >= 50:
-		channel_msgs["main"]["msgs"].pop(0)
-	else:
-		channel_msgs["main"]["num_msgs"]+=1
-	msg = {'text':data['msg'],'username':data['username'],"time":data["time"]}
-	channel_msgs["main"]["msgs"].append(msg)
-	emit("joined",{"success":True,"channel_msgs":channel_msgs[channel]},room=room)
+	emit("joined",{"success":True,"channel_msgs":channel_msgs[channel]['msgs']},room=request.sid)
+
+@socket.on("leave")
+def leave(data):
+	room = data['channel']
+	leave_room(room)
+
+	update_room_msg(data,room)
+	emit('left',{'channel',channels_msgs[room]['msgs']},room=room)
 
 @socket.on("send_to_main")
 def submit_to_all(data):
-	msg = {'text':data['msg'],'username':data['username'],"time":data["time"]}
-	if channels_msgs["main"]["num_msgs"] >= 50:
-		channel_msgs["main"]["msgs"].pop(0)
-	else:
-		channel_msgs["main"]["num_msgs"]+=1
+	update_room_msg(data,"main")
+	emit("send_to_main",{"channel_msgs":channels_msgs['main']['msgs']},broadcast=True)
 
-	channel_msgs["main"]["msgs"].append(msg)
-	emit("send_to_main",{"channel_msgs":channels_msgs},broadcast=True)
+@socket.on("submit_to_room")
+def submit_to_room(data):
+	room=data['channel']
+	update_room_msg(data,room)
+	emit("announce_room",{"channel_msgs":channels_msgs[room]['msgs']})
 
 @socket.on("rejoin_general")
 def rejoin_general():
-	emit("announce_to_main",{"channel_msgs":channels_msgs},broadcast=True)
+	emit("announce_to_main",{"channel_msgs":channels_msgs['main']['msgs']},broadcast=True)
 	
 @socket.on("create_channel")
 def create_channel(data):
@@ -99,18 +172,89 @@ def create_channel(data):
 	channel=data["channel"]
 	if channel == "" or channel is None:
 		error_str="No data received"
-	elif channel in channels_list:
+	elif channel in channels_list or channel in private_channels:
 		error_str="Channel cannot already exist"
 	else:
 		channels_list.append(channel)
+
 		channels_msgs[channel]={'num_msgs':0,msgs:[]}
 	if error_str != "":
 		send("add_channel",{"channel":channel,"error":error_str},room=request.sid)
 	else:
-		emit("add_channel",{"channel":channel},broadcast=True)
+		emit("add_channel",{"channel":channel,"error":""},broadcast=True)
 
+@socket.on("new_user")
+def new_user(data):
+	username=data.get('username')
+	error=""
+	if username is None:
+		error = "No username provided."
+	elif data['username'] in users:
+		error = "Username already exists."
+	else:
+		users[username] = request.sid
+		users_rooms['username']=[]
+
+	if error != "":
+		emit('add_user',{'username':username,'error':''},broadcast=True)
+	else:
+		emit('add_user',{'username':username,'error':error},room=request.sid)
+
+@socket.on("create_private_channel")
+def create_private_channel(data):
+	channel_name=data.get('channel_name')
+	full_name=channel_name+request.sid[0:8]
+	password=data.get('password')
+	msg={"msg":"","error":""}
+	if channel_name is None or channel_name == "":
+		msg["error"]="No channel name given."
+	elif channel_name in channels_list:
+		msg["error"] = "Channel name must be unique and not mirror any of the generals."
+	elif full_name in private_channels["names"]:
+		msg["error"] = "Error your channel name must be unique."
+	else:
+		private_channels["names"][full_name]=channel_name
+		private_channels["passwords"][full_name]=password
+		socket.server.enter_roomt(request.sid,full_name)
+		msg['password']=password
+		msg['msg']=f"Channel {channels_name} created with password {password}"
+		users_rooms['username'].append(full_name)
+
+	emit("private_channel",msg,room=request.sid)
+
+@socket.on('create_pm_room')
+def create_pm_room(data):
+	sender=data.get('username')
+	receiver=data.get('to_username')
+	if receiver not in users:
+		pass
+	pass
+
+@socket.on("send_pm")
+def private_msg(data):
+	sender=data.get('username')
+	receiver=data.get('to_user')
+	if receiver not in users:
+		emit("pm",{'error':'User not found','msg':''},room=request.sid)
+	else:
+		room=users[receiver]+users[sender]
+		channels_msgs[room]={
+			msgs:[],
+			num_msgs:[]
+		}
+		socket.server.enter_room(users[receiver],room)
+		socket.server.enter_room(request.sid,room)
+		#message = {'text': data['msg'], 'sender': sender, 'time': data['time'],'error':''}
+		message={'room_name':room,'from':sender,'to':receiver}
+		emit("pm",message,room=room)
 
 @socket.on("update_user_channels")
-def pass1():
+def update_user_channels(data):
 	pass
 		
+@socket.on("client_disconnect")
+def client_disconnect(data):
+	username=data.get('username')
+	if username is not None:
+		emit("user_left",{'username':username})
+	pass
--- web/assignment_2/application/static/js/script.js
+++ web/assignment_2/application/static/js/script.js
@@ -1,45 +1,306 @@
+//
+
+Array.prototype.removeItem=function(needle){
+	let index = this.indexOf(needle)
+	if(index > -1){
+		return this.splice(index,1);
+	}
+}
+
+Date.prototype.toLocalTime=function() {
+	return this.toLocaleString('en-us', {timeZone: 'America/New_York'})
+}
+
+//the globals
 var g_channels = [];
 var g_users = [];
 var g_current_channel = "";
-var msg_type = "PUBLIC";
+var g_private = {'names':[],'passwords':[]};
+var g_username = localStorage.getItem('username')
+var socket;
+var g_private = false;
+$('body').ready(()=>{
 
-function construct_message(msg_obj){
-	let msg= '<div class="row">'
-	msg+='('+msg_obj['ts']+') '+msg_obj['user']+': '+msg_obj['msg']
-	msg+='</div>'
+	socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+
+	if (!g_username){
+		$('#main_modal').modal('toggle');
+		$('#modal_input').data('type','username');
+	}
+	else{
+		add_user(g_username)
+		socket.emit('rejoin',{'username':g_username});
+	}
+
+	window.onbeforeunload = function () {
+		socket.emit('client_disconnect', {'username':localStorage.getItem('username')});
+	}
+
+	$("#modal_input").on('keyup', function (key) {
+		if (document.getElementById('modal_input').value.length > 0) {
+			$("#modal_button").attr('disabled', false);
+
+			if (key.keyCode === 13) {
+				$('#modal_button').click();
+			}
+		}
+		else {
+			$("#modal_button").attr('disabled', true);
+		}
+	});
+
+
+	$('#modal_button').on('click',()=>{
+		let input=$("#modal_input");
+		let input_val=input.val();
+		switch(input){
+			case "username":
+				//let username=input.val();
+				if(input_val in g_users){
+					$('#main_modal_title').text('This user already exists. Please select another.');
+				}
+				else {
+					socket.emit('new_user', {'username': input_val});
+				}
+				break;
+
+			case "channel":
+				//let name=input.val();
+				if(input_val in g_channels || input_val in g_private){
+					$('#main_modal_title').text('This channel already exists. Please select another name.');
+				}
+				else {
+					socket.emit('new_channel',{'channel':input_val});
+				}
+				break;
+
+			case "private_channel":
+				//let name=input.val();
+				if(input_val in g_channels || input_val in g_private){
+					$('#main_modal_title').text('This channel already exists. Please select another name.');
+				}
+				else {
+					let el=document.getElementById('channel_password');
+					let password=el.value;
+					el.value='';
+					socket.emit('create_private_channel',{'name':input_val,'password':password})
+				}
+				break;
+		}
+
+	});
+
+	$('#add_channel').on('click',()=>{
+		$('#main_modal_title').text('Please enter the channel name');
+		$('#main_modal_label').text('Channel Name:')
+		let div='<div class="col"><input type="checkbox" id="private_channel_toggle" onclick=""></div>'
+		$('#input_row').after(div);
+		$('#private_channel_toggle').on('click',()=>{
+			if(this.prop('checked')){
+				$('#password_row').remove();
+			}
+			else{
+				$('#input_row').after('<div class="row" id="password_row"><div class="col"><input type="text" id="channel_password"></div></div>')
+			}
+		});
+		$('#main_modal').modal('toggle');
+	})
+
+	$('#join_private_channel').on('click',()=>{
+		$('#main_modal_title').text('Please enter the channel\'s name');
+		$('#main_modal_label').text('Channel Name:')
+		$('#input_row').after(`<div class="row" id="password_row"><div class="col"><label for="channel_password">Channel Password
+			</label><input id="channel_password" type="text"></div></div>`);
+		$('#main_modal').modal('toggle');
+	});
+
+	socket.on("add_user",data=>{
+		if (data['error'] == '') {
+			$('#input_row').innerHTML(`<div class="col"><label for="modal_input" id="modal_label">Username</label>
+				<input id="modal_input" class="form-control w-50" type="text" data-type=""></div>`)
+			$('#main_modal').modal('toggle');
+			$('#main_modal_title').txt(data['error']);
+		}
+		else {
+			if( (data['username'] != g_username) || (data['username'] != localStorage.getItem('username')) ){
+				add_user(data['username']);
+			}
+			else{
+				localStorage.setItem('username', data['username']);
+			}
+		}
+	});
+
+	socket.on('add_channel',data=>{
+		if(data['error'] != ''){
+			$('#main_modal').modal('toggle');
+			$('#main_modal_title').txt(data['error']);
+		}
+		else{
+			add_channel(data['channel']);
+		}
+	});
+	socket.on('private_channel',data=>{
+		if(data['error']!=''){
+			$('main_modal').modal('toggle');
+			$('#main_modal_title').txt(data['error']);
+		}
+		else{
+			g_private['names'].push(data['channel']);
+			g_private['password'].push(data['password']);
+			add_channel(data['channel'],true);
+		}
+	});
+
+	socket.on('user_left',data=>{
+		g_users.removeItem(data['username'])
+		let username=data['username'].replace(' ','_');
+		$('#'+username).remove()
+	});
+
+	socket.on('joined',data=>{
+		if(data['success']){
+			add_messages(data['channel_msgs']);
+		}
+	});
+
+
+	socket.on("update_channels",data=>{
+		let channels=data['channels'];
+		g_channels=channels;
+		let max=channels.length;
+
+		for(let i=0;i<max;i++){
+			add_channel(channels[i],false);
+		}
+		//makes the first channel the active one as that's always the one that they join.
+		$(`#${g_channels[0]}`).addClass('active');
+		channels=data['private_channels'];
+		if(channels.length >= 1) {
+			for (const full_name in channels) {
+				g_private['names'].push(channels[full_name]['name'])
+				g_private['passwords'].push(channels[full_name]['password']);
+				add_private_channel(channels[full_name]['name'], full_name, channels['password']);
+			}
+		}
+	})
+
+	function pm_user(username_id){
+		let username=document.getElementById(username_id).innerText;
+		let msg={'username':g_username,'to_user':username}
+		if(username in g_users){
+			socket.emit('send_pm',msg);
+		}
+		else{
+
+		}
+	}
+
+});
+function change_channel(channel){
+	let private=$(`#${channel}`).data('private');
+	let password='';
+	if(private){
+		let index=g_private['names'].indexOf(channel);
+		password=g_private['passwords'][index];
+	}
+	$(`#${channel}`).removeClass('active');
+	g_current_channel = channel;
+	$(`#${channel}`).addClass('active');
+	socket.emit('join',{'room':g_current_channel,'password':password});
 }
 
-function get_msgs(route,type,data){
+function add_messages(messages){
+	let total_messages=messages.length;
+	let msgs=''
+	for(let i=0;i<total_messages;i++){
+		msgs+=contstruct_message(messages[i]);
+	}
+	$('#msg_block').html(msgs);
+}
+function construct_message(msg_obj) {
+		let user_id=msg_obj['username'].replace(' ','_');
+		let msg = `<div class="media d-flex"><div>
+		<span><a id="${user_id}" href="#" onclick="message_user(this.id)">${msg_obj['username']}</a></span><span class="pl-2">
+			@ ${Date(msg_obj['ts']).toLocalTime()}</span><p>${msg_obj['msg']}</p></div></div>`;
+		//msg += '(' + msg_obj['ts'] + ') ' + msg_obj['user'] + ': ' + msg_obj['msg']
+		//msg += '</div>'
+	return msg;
+}
+
+function get_msgs(route, type, data) {
 	const req = new XMLHttpRequest();
-	req.open(type,route);
+	req.open(type, route);
 	req.send(data)
-	req.onload = () =>{
+	req.onload = () => {
 		let msgs = '';
 		const json_data = JSON.parse(req.responseText)
-		if(json_data.success){
+		if (json_data.success) {
 			let total_msgs = json_data.length;
-			for(let i=0;total_msgs;i++){
-				msgs+=construct_message(msg_obj[i]);
+			for (let i = 0;i<total_msgs; i++) {
+				msgs += construct_message(msg_obj[i]);
 			}
 			document.getElementById('messages').innerHTML = msgs;
 		}
 	}
 }
-function unbold_channel(){
+
+function unbold_channel() {
 	document.getElementById(g_current_channel).style.fontWeight = "normal";
 }
+
 function bold_channel(){
 	document.getElementById(g_current_channel).style.fontWeight = "bold";
 }
 
-function change_channel(channel){
-	if(g_channels.includes(g_current_channel)){
-		unbold_channel();
+
+function add_channel(channel_name,private=false){
+	const li=document.createElement('li');
+	li.innerText = '#'+channel_name;
+	let channel=channel_name.replace(' ','_');
+	li.setAttribute('id',channel);
+	li.setAttribute('class','list-group-item list-group-item-dark');
+	li.setAttribute('onclick','change_channel(this.id)');
+	li.setAttribute('data-private',`${private}`);
+	$('#channels').append(li);
+}
+
+function add_private_channel(short_name,full_name,password){
+	const li=document.createElement('li');
+	li.innerText = '#'+channel_name;
+	let channel=full_name.replace(' ','_');
+	li.setAttribute('id',channel);
+	li.setAttribute('class','list-group-item list-group-item-dark');
+	li.setAttribute('onclick','change_channel(this.id)');
+	li.setAttribute('data-private',true);
+	$('#channels').append(li);
+}
+
+function add_user(username){
+	const p=document.createElement('p');
+	p.innerText=username;
+	p.setAttribute('id',username.replace(' ','_'))
+	p.setAttribute('onclick','message_user(this.id)');
+	$('#users_online').append(p);
+}
+function send_msg(){
+	let msg = $('#input_box').val;
+	if(g_private){
+		socket.emit("send_pm");
 	}
-	g_current_channel = channel;
-	msg_type = "PUBLIC";
-	bold_channel();
-	document.getElementById('current_channel').innerText = g_current_channel
-	localStorage.setItem("channel",g_current_channel);
-	get_msgs('/get_messages','POST',channel)
+}
+function select_user(){
+
+}
+function check_msg(event){
+	let key=event.key;
+	if (document.getElementById('modal_input').value.length > 0) {
+		$("#modal_button").attr('disabled', false);
+		if (key === "Enter") {
+			send_msg();
+		}
+	}
+	else {
+		$("#modal_button").attr('disabled', true);
+	}
 }
